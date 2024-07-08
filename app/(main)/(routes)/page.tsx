ae8a97e8c6b217d93be8f3f7fa1725fcbce8164e
YapYanTing <yanting@boostorder.com>
2024-07-08 15:53:23 +0800

    Setup theme component

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,11 @@
 import { UserButton } from "@clerk/nextjs";
+import { ModeToggle } from "@/components/mode-toggle";
 
 export default function Home() {
   return (
     <div>
       <UserButton />
+      <ModeToggle />
     </div>
   );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import "./globals.css";
 import type { Metadata } from "next";
+import { cn } from "@/lib/utils";
 import { Open_Sans } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/providers/theme-provider";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -17,8 +19,22 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body className={font.className}>{children}</body>
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={cn(
+            font.className,
+            "bg-white dark:bg-[#313338]"
+          )}
+        >
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem={false}
+            storageKey="discord-theme"
+          >
+            {children}
+          </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
